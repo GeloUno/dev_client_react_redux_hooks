893feca6737beff521b208ab943ab7dbf7c8d729
GeloUno <geloke59@gmail.com>
2019-08-08 11:28:34 +0200

    Protected Route
    --------------------------------------
    add private route for login user and redirect for guest
    --------------------------------------

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import NavBar from './component/Layout/NavBar';
 import Landing from './component/Layout/Landing';
 import Login from '../src/component/Auth/Login';
 import Register from '../src/component/Auth/Register';
+import Dashboard from './component/dashboard/Dashboard';
 import Alert from './component/Auth/Alert';
+import PrivateRoute from './component/routing/PrivateRoute';
 
 import './App.css';
 
@@ -12,7 +14,9 @@ import './App.css';
 import { Provider } from 'react-redux';
 import store from './store';
 import setAuthToken from './utils/token';
+
 import { loadUser } from './action/auth';
+
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -29,12 +33,13 @@ const App = () => {
       <Router>
         <Fragment>
           <NavBar />
-          <Route exact path="/" component={Landing} />
+          <Route exact path="/" component={Landing}></Route>
           <section className="container">
             <Alert />
             <Switch>
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login}></Route>
+              <Route exact path="/register" component={Register}></Route>
+              <PrivateRoute exact path="/dashboard" component={Dashboard}></PrivateRoute>
             </Switch>
           </section>
         </Fragment>
