b22d31d66879e5498c4b143cfaae2f43aa3dd6d2
GeloUno <geloke59@gmail.com>
2020-05-14 16:44:22 +0200

    Updates

---
+++ precache-manifest.d91f98d5984a9867dd269c96c3170c11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6fac0df994422ca80bf3501de58f3f1",
+    "url": "/dev_client_react_redux_hooks/index.html"
+  },
+  {
+    "revision": "59adc06bc5ed22a9ad14",
+    "url": "/dev_client_react_redux_hooks/static/css/main.987bf08f.chunk.css"
+  },
+  {
+    "revision": "13992400cbf5325b8cfc",
+    "url": "/dev_client_react_redux_hooks/static/js/2.0f8a2bca.chunk.js"
+  },
+  {
+    "revision": "59adc06bc5ed22a9ad14",
+    "url": "/dev_client_react_redux_hooks/static/js/main.1ee839df.chunk.js"
+  },
+  {
+    "revision": "fe8a82d7961ec5223ecd",
+    "url": "/dev_client_react_redux_hooks/static/js/runtime~main.c378502c.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/dev_client_react_redux_hooks/static/media/showcase.4b31330b.jpg"
+  }
+]);
