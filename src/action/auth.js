b077cd7734e0efdfe17298e9a33bd34b3367c442
GeloUno <geloke59@gmail.com>
2020-05-19 17:30:12 +0200

    no fix

--- src/action/auth.js
+++ src/action/auth.js
@@ -7,36 +7,36 @@ import {
   USER_LOGIN,
   USER_FAIL,
   USER_LOGOUT,
-  PROFILE_CLEAR
+  PROFILE_CLEAR,
 } from './types';
 import { setAlert } from './alert';
 import setAuthToken from './../utils/token';
 
-export const loadUser = () => async dispatch => {
+export const loadUser = () => async (dispatch) => {
   if (localStorage.token) {
     try {
       setAuthToken(localStorage.token);
       const res = await axios.get('/api/auth');
       dispatch({
         type: USER_LOAD,
-        payload: res.data
+        payload: res.data,
       });
     } catch (err) {
       dispatch({
-        type: USER_ERROR
+        type: USER_ERROR,
       });
     }
   } else {
     dispatch({
-      type: USER_ERROR
+      type: USER_ERROR,
     });
   }
 };
-export const register = ({ name, email, password }) => async dispatch => {
+export const register = ({ name, email, password }) => async (dispatch) => {
   const config = {
     headers: {
-      'Content-Type': 'application/json'
-    }
+      'Content-Type': 'application/json',
+    },
   };
   const body = JSON.stringify({ name, email, password });
 
@@ -44,25 +44,24 @@ export const register = ({ name, email, password }) => async dispatch => {
     const res = await axios.post('/api/users', body, config);
     dispatch({
       type: REGISTER_SUCCESS,
-      payload: res.data
+      payload: res.data,
     });
     dispatch(loadUser());
-  } 
-  catch (err) {
+  } catch (err) {
     const errors = err.response.data.errors;
     if (errors) {
-      errors.forEach(element => dispatch(setAlert(element.msg, 'danger')));
+      errors.forEach((element) => dispatch(setAlert(element.msg, 'danger')));
     }
     dispatch({
-      type: REGISTER_FAIL
+      type: REGISTER_FAIL,
     });
   }
 };
-export const login = (email, password) => async dispatch => {
+export const login = (email, password) => async (dispatch) => {
   const config = {
     headers: {
-      'Content-Type': 'application/json'
-    }
+      'Content-Type': 'application/json',
+    },
   };
   const body = JSON.stringify({ email, password });
 
@@ -70,25 +69,24 @@ export const login = (email, password) => async dispatch => {
     const res = await axios.post('/api/auth', body, config);
     dispatch({
       type: USER_LOGIN,
-      payload: res.data
+      payload: res.data,
     });
     dispatch(loadUser());
-  } 
-  catch (err) {
+  } catch (err) {
     const errors = err.response.data.errors;
     if (errors) {
-      errors.forEach(element => dispatch(setAlert(element.msg, 'danger')));
+      errors.forEach((element) => dispatch(setAlert(element.msg, 'danger')));
     }
     dispatch({
-      type: USER_FAIL
+      type: USER_FAIL,
     });
   }
 };
-export const userLogout = () => dispatch => {
+export const userLogout = () => (dispatch) => {
   dispatch({
-    type: PROFILE_CLEAR
+    type: PROFILE_CLEAR,
   });
   dispatch({
-    type: USER_LOGOUT
+    type: USER_LOGOUT,
   });
 };
--- src/action/posts.js
+++ src/action/posts.js
@@ -5,50 +5,50 @@ import {
   POST_DELETE,
   POST_GET,
   POST_GET_ERROR,
-  POST_COMMENT_ADD
+  POST_COMMENT_ADD,
 } from './types';
 import axios from 'axios';
 
-export const getAllPost = () => async dispach => {
+export const getAllPost = () => async (dispach) => {
   try {
     const res = await axios.get('api/post');
     //  console.log(res.data[0]);
 
     dispach({
       type: POSTS_GET,
-      payload: res.data[0]
+      payload: res.data[0],
     });
   } catch (error) {
     console.log(error.respon);
 
     dispach({
       type: POSTS_GET_ERROR,
-      payload: error
+      payload: error,
     });
   }
 };
-export const getPost = id => async dispach => {
+export const getPost = (id) => async (dispach) => {
   try {
     const res = await axios.get(`/api/post/${id}`);
 
     dispach({
       type: POST_GET,
-      payload: res.data
+      payload: res.data,
     });
   } catch (error) {
     console.log(error.respon);
 
     dispach({
       type: POST_GET_ERROR,
-      payload: error
+      payload: error,
     });
   }
 };
-export const addPost = text => async dispach => {
+export const addPost = (text) => async (dispach) => {
   const config = {
     header: {
-      'Content-Type': 'aplication/json'
-    }
+      'Content-Type': 'aplication/json',
+    },
   };
   try {
     const res = await axios.post('api/post', text, config);
@@ -56,20 +56,20 @@ export const addPost = text => async dispach => {
 
     dispach({
       type: POST_ADD,
-      payload: res.data
+      payload: res.data,
     });
   } catch (error) {
     console.log(error.data);
   }
 };
 
-export const deletePost = id => async dispach => {
+export const deletePost = (id) => async (dispach) => {
   // console.log('DELETE POST TRY '+ id);
   try {
     await axios.delete(`api/post/${id}`);
     dispach({
       type: POST_DELETE,
-      payload: id
+      payload: id,
     });
   } catch (error) {
     console.log('error delete post');
@@ -77,11 +77,11 @@ export const deletePost = id => async dispach => {
   }
 };
 
-export const addCommentPost = (text, id) => async dispach => {
+export const addCommentPost = (text, id) => async (dispach) => {
   const config = {
     header: {
-      'Content-Type': 'aplication/json'
-    }
+      'Content-Type': 'aplication/json',
+    },
   };
   console.log('text');
   console.log(text);
@@ -91,7 +91,7 @@ export const addCommentPost = (text, id) => async dispach => {
 
     dispach({
       type: POST_COMMENT_ADD,
-      payload: res.data.comments
+      payload: res.data.comments,
     });
   } catch (error) {
     console.log(error.data);
